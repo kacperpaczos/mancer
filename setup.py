559ea06485f170775bb529465325fd81daede2b7
Kacper Paczos <kacper.paczos@linux.pl>
2025-11-02 18:41:29 +0100

    chore(release): bump version to 0.7.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_namespace_packages, setup
 
 setup(
     name="mancer",
-    version="1.7.1",
+    version="0.7.1",
     package_dir={"": "src"},
     packages=find_namespace_packages(where="src"),
     description="Programmable automation engine - execute commands on any platform (Linux/Windows/macOS) locally or remotely through one Python interface",
